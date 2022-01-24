1d2e9ceac23a23e733abee5622eb1647078ce150
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2022-01-24 09:13:49 -0300

    Initial commit + tailwindcss config

---
+++ components/Login.js
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div className="bg-black relative text-white">
+      <h1>Login</h1>
+    </div>
+  );
+}
+
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
 import Head from 'next/head';
+import Login from '../components/Login';
 
 export default function Home() {
+  const isAuthenticated = false;
+
+  if (!isAuthenticated) return <Login />;
+
   return (
-    <div className="">
+    <div className="h-screen">
       <Head>
         <title>Dapp Chat</title>
         <link rel="icon" href="/favicon.ico" />
 
         <h1>Welcome to the descentralized universe</h1>
       </Head>
+
+      <Login />
     </div>
   );
 }
